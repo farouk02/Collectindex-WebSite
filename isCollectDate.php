96e99f3ccadcbb5e18c55fda7bbc4bc5507ff38a
farouk02 <faroukious0@gmail.com>
2021-05-16 00:53:34 +0100

    is collect day

---
+++ isCollectDate.php
@@ -0,0 +1,29 @@
+<?php
+require "DataBaseConfig.php";
+
+$dbC = new DataBaseConfig();
+
+$servername = $dbC->servername;
+$dbusername = $dbC->username;
+$dbpassword = $dbC->password;
+$dbname = $dbC->databasename;
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_errno) {
+  die("connection faild: " . $conn->connect_errno);
+}
+
+$mounth = Date("m");
+$day = Date("d");
+
+
+$heroes = array();
+
+$sql = "SELECT id FROM collect_date WHERE mounth = '" . $mounth . "' AND '" . $day . "' BETWEEN start_day - 1 AND end_day + 1 ";
+
+if ($conn->query($sql)->fetch_assoc()) {
+  echo "1";
+} else {
+  echo "0";
+}
